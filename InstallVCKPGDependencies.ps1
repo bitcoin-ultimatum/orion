48a9afc13ba684e6eaeb3840fba7633a1a7a87ef
Aleksandr Belets <askii.robotics@gmail.com>
2021-02-16 20:29:42 +0300

    updated ps vckpg ps scripts

--- InstallVCKPGDependencies.ps1
+++ InstallVCKPGDependencies.ps1
@@ -1,17 +1,18 @@
 ﻿$ErrorActionPreference = "Stop"
 
 Write-Host "Installing vcpkg dependencies..." -ForegroundColor Cyan
-
+cd vcpkg
 Write-Host "Step 1. Downloading list of x86-windows dependencies..." -ForegroundColor Cyan
 (new-object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/bitcoin-ultimatum/orion/master/depends/vcpkg/vcpkg-packages-prerequisites-86.txt","${PWD}\vcpkg-packages-prerequisites-86.txt")
 
 Write-Host "Step 2. Installing x86-windows dependencies..." -ForegroundColor Cyan
-vcpkg install --triplet x86-windows $(Get-Content -Path vcpkg-packages-prerequisites-86.txt).split()
+./vcpkg install --triplet x86-windows $(Get-Content -Path vcpkg-packages-prerequisites-86.txt).split()
 
 Write-Host "Step 3. Downloading list of x64-windows-static dependencies..." -ForegroundColor Cyan
 (new-object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/bitcoin-ultimatum/orion/master/depends/vcpkg/vcpkg-packages.txt","${PWD}\vcpkg-packages.txt")
 
 Write-Host "Step 4. Installing x64-windows-static dependencies..." -ForegroundColor Cyan
-vcpkg install --triplet x64-windows-static $(Get-Content -Path vcpkg-packages.txt).split()
+./vcpkg install --triplet x64-windows-static $(Get-Content -Path vcpkg-packages.txt).split()
 
 Write-Host "Installing vcpkg dependencies... Done!" -ForegroundColor Cyan
+cd ..
--- InstallVCPKG.ps1
+++ InstallVCPKG.ps1
@@ -1,26 +1,30 @@
-﻿$ErrorActionPreference = "Stop"
+﻿if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+$ErrorActionPreference = "Stop"
+$env:GIT_REDIRECT_STDERR = '2>&1'
 Try {
     Write-Host "Step 1. Cloning vcpkg..." -ForegroundColor Cyan
-    Try {
-        git clone https://github.com/Microsoft/vcpkg.git
-    } Catch  {}
+    git clone https://github.com/Microsoft/vcpkg.git;
     cd vcpkg
-    Write-Host "Step 2. Bootstrapping vcpkg..." -ForegroundColor Cyan
-    PowerShell.exe -ExecutionPolicy Bypass -File ".\scripts\bootstrap.ps1" 2>&1>$null
-    cmd ./bootstrap-vcpkg.bat        # Installation script
-    Write-Host "Step 3. Integration vcpkg with MSVC..." -ForegroundColor Cyan
-    ./vcpkg integrate install    # Integration with MSVC
+    & Write-Host "Step 2. Bootstrapping vcpkg..." -ForegroundColor Cyan
+    & Start-Process -Verb RunAs "cmd.exe"  "/c ${PWD}/bootstrap-vcpkg.bat" -Wait
+    & Write-Host "Step 3. Integration vcpkg with MSVC..." -ForegroundColor Cyan
+    & Start-Process -Verb RunAs "${PWD}/vcpkg.exe"  "integrate instal" -Wait
 
     Write-Host "Step 4. Adding env variabled..." -ForegroundColor Cyan
     setx VCPKG_ROOT "${PWD}" /M  # Set env variable VCPKG_ROOT
     setx VCPKG_DEFAULT_TRIPLET "x64-windows-static" /M
             # The default supported build configuration is
             # x64 build with static dependencies
-    refreshenv                   # Update environment variables
 
 
     Write-Host "Adding vcpkg to PATH..." -ForegroundColor Cyan
-    Add-Path "${PWD}"
+    setx PATH "$env:path;${PWD}" -m
     Write-Host "Installing vcpkg... Done!" -ForegroundColor Cyan
 } ### End Try block
 Catch  {
